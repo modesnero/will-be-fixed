97586f1aa5d510963683b506efd6473dc7ab6c12
modesnero <modesnero@gmail.com>
2020-06-06 14:36:25 +0300

    fix register

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
   vote: { type: Number, required: true },
-  myVoteEmail: { type: String, required: true },
+  myVoteEmail: { type: String, required: false },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
